e9240dbbcf88dc67e9721fd5a47f78c2f2ec494a
Nishanthan-P <nishanthan.erode@gmail.com>
2018-12-15 12:13:54 +0530

    first commit

---
+++ JavaInterviewPrograms/src/com/example/iterview/programs/SwapTwoNum.java
@@ -0,0 +1,15 @@
+package com.example.iterview.programs;
+
+public class SwapTwoNum {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		System.out.println(a +" "+ b);
+		a = a+b;//a=30
+		b=a-b;//b=10
+		a=a-b;//a=20
+		System.out.println(a+" "+b);
+	}
+}
